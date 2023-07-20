8a25481a71a32b53069121c71fc7b82843ae2336
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-07-20 08:33:12 +0300

    add overlay

---
+++ client/src/shared/ui/overlay/Overlay.tsx
@@ -0,0 +1,19 @@
+import { FC, memo } from 'react'
+import { classNames } from '../../lib/classNames/classNames'
+import cls from './Overlay.module.scss'
+
+interface OverlayProps {
+  className?: string
+  onClick?: () => void
+}
+
+export const Overlay: FC<OverlayProps> = memo((props) => {
+  const { className, onClick } = props
+
+  return (
+    <div
+      onClick={onClick}
+      className={classNames(cls.overlay, {}, [className])}
+    />
+  )
+})
