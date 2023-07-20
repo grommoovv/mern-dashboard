ee9ce537041c3bfd9c44c82a8df8a5438c4f4767
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-07-20 08:35:53 +0300

    add classNames

---
+++ client/src/shared/lib/classNames/classNames.ts
@@ -0,0 +1,15 @@
+export type ModsType = Record<string, boolean | string | undefined>;
+
+export function classNames(
+    cls: string,
+    mods: ModsType = {},
+    additional: Array<string | undefined> = [],
+): string {
+    return [
+        cls,
+        ...additional.filter(Boolean),
+        ...Object.entries(mods)
+            .filter(([_, value]) => Boolean(value))
+            .map(([className]) => className),
+    ].join(' ');
+}
